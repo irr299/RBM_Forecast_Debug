d13472f40187370cf4291bfc655ec91f4c4037c6
IRR Johnson <johnson@gfz.de>
2026-01-09 23:59:27 +0100

    Add new file

---
+++ ace_optimisation /get_realtime_ace_sw_mag_from_mat.m
@@ -0,0 +1,94 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Written by I. Michaelis (GFZ), 2019
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function  [sw,mag] = get_realtime_ace_sw_mag_from_mat(path_ace,matlabd,prop,time_start,time_end)
+    sw.matlabd=[];sw.jd=[];sw.stat=[];sw.swn=[];sw.swv=[];sw.swT=[];
+    mag.matlabd=[];mag.jd=[];mag.stat=[];mag.bx=[];mag.by=[];mag.bz=[];mag.bt=[];mag.lat=[];mag.lon=[];
+    in_sw.matlabd=[];in_sw.jd=[];in_sw.stat=[];in_sw.swn=[];in_sw.swv=[];in_sw.swT=[];
+    in_mag.matlabd=[];in_mag.jd=[];in_mag.stat=[];in_mag.bx=[];in_mag.by=[];in_mag.bz=[];in_mag.bt=[];in_mag.lat=[];in_mag.lon=[];
+    if ~isempty(matlabd)
+        time_start=floor(min(matlabd));
+        time_end=ceil(max(matlabd));
+    else
+        matlabd=time_start:time_end;
+    end
+    for day=time_start:time_end
+        in_sw_file=strcat(path_ace,"swepam_1m/",datestr(datenum(day),'YYYYmm'),"/","ace_swepam_1m_",datestr(datenum(day),'YYYYmmdd'),".mat");
+        fprintf('%s\n',in_sw_file);
+        if exist(in_sw_file)
+            data=load(in_sw_file);
+            in_sw.matlabd=cat(1,in_sw.matlabd,data.data.matlabd);
+            in_sw.jd=cat(1,in_sw.jd,data.data.jd);
+            in_sw.stat=cat(1,in_sw.stat,data.data.status);
+            in_sw.swn=cat(1,in_sw.swn,data.data.swn);
+            in_sw.swv=cat(1,in_sw.swv,data.data.swv);
+            in_sw.swT=cat(1,in_sw.swT,data.data.swT);
+        end
+
+        in_mag_file=strcat(path_ace,"/mag_1m/",datestr(datenum(day),'YYYYmm'),"/","ace_mag_1m_",datestr(datenum(day),'YYYYmmdd'),".mat");
+        fprintf('%s\n',in_mag_file);
+        if exist(in_sw_file)
+            data=load(in_mag_file);
+            in_mag.matlabd=cat(1,in_mag.matlabd,data.data.matlabd);
+            in_mag.jd=cat(1,in_mag.jd,data.data.jd);
+            in_mag.stat=cat(1,in_mag.stat,data.data.status);
+            in_mag.bx=cat(1,in_mag.bx,data.data.bx);
+            in_mag.by=cat(1,in_mag.by,data.data.by);
+            in_mag.bz=cat(1,in_mag.bz,data.data.bz);
+            in_mag.bt=cat(1,in_mag.bt,data.data.bt);
+            in_mag.lat=cat(1,in_mag.lat,data.data.lat);
+            in_mag.lon=cat(1,in_mag.lon,data.data.lon);
+        end
+    end
+    
+    if prop
+        [sw_prop,mag_prop] = ace_sw_mag_prop_simple( ...
+                            cat(2,in_sw.matlabd,in_sw.jd,in_sw.stat,in_sw.swn,in_sw.swv,in_sw.swT), ...
+                            cat(2,in_mag.matlabd,in_mag.jd,in_mag.stat,in_mag.bx,in_mag.by,in_mag.bz,in_mag.bt,in_mag.lat,in_mag.lon) ...
+                            );
+        sw.matlabd=matlabd;
+        sw.jd=interp1(sw_prop(:,1),sw_prop(:,2),sw.matlabd,'linear');
+        sw.stat=interp1(sw_prop(:,1),sw_prop(:,3),sw.matlabd,'nearest');
+        sw.swn=interp1(sw_prop(:,1),sw_prop(:,4),sw.matlabd,'linear');
+        sw.swv=interp1(sw_prop(:,1),sw_prop(:,5),sw.matlabd,'linear');
+        sw.swT=interp1(sw_prop(:,1),sw_prop(:,6),sw.matlabd,'linear');
+
+        mag.matlabd=matlabd;
+        mag.jd=interp1(mag_prop(:,1),mag_prop(:,2),mag.matlabd,'linear');
+        mag.stat=interp1(mag_prop(:,1),mag_prop(:,3),mag.matlabd,'nearest');
+        mag.bx=interp1(mag_prop(:,1),mag_prop(:,4),mag.matlabd,'linear');
+        mag.by=interp1(mag_prop(:,1),mag_prop(:,5),mag.matlabd,'linear');
+        mag.bz=interp1(mag_prop(:,1),mag_prop(:,6),mag.matlabd,'linear');
+        mag.bt=interp1(mag_prop(:,1),mag_prop(:,7),mag.matlabd,'linear');
+        mag.lat=interp1(mag_prop(:,1),mag_prop(:,8),mag.matlabd,'linear');
+        mag.lon=interp1(mag_prop(:,1),mag_prop(:,9),mag.matlabd,'linear');
+        
+    else
+        sw.matlabd=matlabd;
+        sw.jd=interp1(in.matlabd,in_sw.jd,matlabd,'linear');
+        sw.stat=interp1(in.matlabd,in_sw.stat,matlabd,'nearest');
+        sw.swn=interp1(in.matlabd,in_sw.swn,matlabd,'linear');
+        sw.swv=interp1(in.matlabd,in_sw.swv,matlabd,'linear');
+        sw.swT=interp1(in.matlabd,in_sw.swT,matlabd,'linear');
+
+        mag.matlabd=matlabd;
+        mag.jd=interp1(in_mag.matlabd,in_mag.jd,matlabd,'linear');
+        mag.stat=interp1(in_mag.matlabd,in_mag.stat,matlabd,'nearest');
+        mag.bx=interp1(in_mag.matlabd,in_mag.bx,matlabd,'linear');
+        mag.by=interp1(in_mag.matlabd,in_mag.by,matlabd,'linear');
+        mag.bz=interp1(in_mag.matlabd,in_mag.bz,matlabd,'linear');
+        mag.bt=interp1(in_mag.matlabd,in_mag.bt,matlabd,'linear');
+        mag.lat=interp1(in_mag.matlabd,in_mag.lat,matlabd,'linear');
+        mag.lon=interp1(in_mag.matlabd,in_mag.lon,matlabd,'linear');
+    end
+return;
+
+% path_ace=getenv("FC_ACE_REALTIME_PROCESSED_DATA_DIR");
+% time_start=datenum(2019,8,15);
+% time_end=datenum(2019,8,22);
+% matlabd=time_start:.5:time_end;
+% prop=0;
+% sw=get_realtime_ace_sw_from_mat(path_ace,matlabd,prop)
+% 
+
